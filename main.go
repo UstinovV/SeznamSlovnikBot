c6bfd5660c0437e8fb5580b282d85b06a0505e3b
ustinov.vd <ustinov.vd@gmail.com>
2019-11-02 17:43:20 +0100

    * .gitignore added * bot basic structure

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	tb "gopkg.in/tucnak/telebot.v2"
+)
+
+func main() {
+	b, err := tb.NewBot(tb.Settings{
+		Token: "",
+		// You can also set custom API URL. If field is empty it equals to "https://api.telegram.org"
+		URL:    "https://api.telegram.org",
+		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
+	})
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	b.Handle("/hello", func(m *tb.Message) {
+		b.Send(m.Sender, "hello world")
+	})
+
+	b.Start()
+}
