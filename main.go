3a72abdbdc8fe71f3b7626d375f58d87128f85d4
UstinovV <ustinov.vd@gmail.com>
2019-11-12 21:28:57 +0100

    Bot API-key as flag

--- main.go
+++ main.go
@@ -3,13 +3,19 @@ package main
 import (
 	"log"
 	"time"
-
+	"flag"
+	"fmt"
 	tb "gopkg.in/tucnak/telebot.v2"
 )
 
 func main() {
+
+	var botKey string
+	flag.StringVar(&botKey, "key", "", "Bot API key")
+	flag.Parse()
+	fmt.Println(botKey)
 	b, err := tb.NewBot(tb.Settings{
-		Token: "",
+		Token: botKey,
 		// You can also set custom API URL. If field is empty it equals to "https://api.telegram.org"
 		URL:    "https://api.telegram.org",
 		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
